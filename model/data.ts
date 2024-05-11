391c1c64b57eca9c95168da2c43f4d729434c7e3
bmancilla23 <bmancilla119@gmail.com>
2024-05-10 21:06:39 -0500

    feat: data creada

---
+++ model/data.ts
@@ -0,0 +1,87 @@
+export interface Song {
+	title: string;
+	artist: string;
+	image: string;
+	id: string;
+}
+
+export const songs: Song[] = [
+	{
+		title: 'X',
+		artist: 'Nicky Jam feat. J Balvin',
+		image:
+			'https://geo-media.beatsource.com/image_size/500x500/c/9/1/c91577c3-118a-42c8-aba3-c6f824085d62.jpg',
+		id: '1',
+	},
+	{
+		title: 'El Ganador',
+		artist: 'Nicky Jam',
+		image:
+			'https://e-cdn-images.dzcdn.net/images/cover/68b6ebdd0514f16c57a87552a33d26d3/500x500-000000-80-0-0.jpg',
+		id: '2',
+	},
+	{
+		title: 'Safaera',
+		artist: 'Bad Bunny feat. Jowell & Randy, Ñengo Flow',
+		image:
+			'https://e-cdn-images.dzcdn.net/images/cover/675d2a54b198cec859e3b38964b71efd/500x500-000000-80-0-0.jpg',
+		id: '3',
+	},
+	{
+		title: 'Travesuras',
+		artist: 'Nicky Jam',
+		image:
+			'https://e-cdn-images.dzcdn.net/images/cover/f51d3135b363ad2455296914b8597735/500x500-000000-80-0-0.jpg',
+		id: '4',
+	},
+	{
+		title: 'Sube la Music',
+		artist: 'De La Ghetto',
+		image: 'https://example.com/sube_la_music.jpg',
+		id: '5',
+	},
+	{
+		title: 'La Formula',
+		artist: 'De La Ghetto feat. Daddy Yankee, Ozuna & Chris Jeday',
+		image:
+			'https://e-cdn-images.dzcdn.net/images/cover/5b929719e82b06cdb786198b4f53ba22/500x500-000000-80-0-0.jpg',
+		id: '6',
+	},
+	{
+		title: 'Me Acostumbre',
+		artist: 'Arcangel feat. Bad Bunny',
+		image:
+			'https://e-cdn-images.dzcdn.net/images/cover/7a18d63b048da5b34763b3d227f593a3/500x500-000000-80-0-0.jpg',
+		id: '7',
+	},
+	{
+		title: 'Flow Cabron',
+		artist: 'Arcangel',
+		image: 'https://example.com/flow_cabron.jpg',
+		id: '8',
+	},
+	{
+		title: 'Numb',
+		artist: 'Linkin Park',
+		image: 'https://example.com/numb.jpg',
+		id: '9',
+	},
+	{
+		title: 'In the End',
+		artist: 'Linkin Park',
+		image: 'https://example.com/in_the_end.jpg',
+		id: '10',
+	},
+	{
+		title: 'Enter Sandman',
+		artist: 'Metallica',
+		image: 'https://example.com/enter_sandman.jpg',
+		id: '11',
+	},
+	{
+		title: 'Nothing Else Matters',
+		artist: 'Metallica',
+		image: 'https://example.com/nothing_else_matters.jpg',
+		id: '12',
+	},
+];
